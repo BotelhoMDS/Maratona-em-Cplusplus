3efc068ffaff4ed3353f4d0fbccb6e074d58b6c1
BotelhoMDS <matheus.botelho@unifei.edu.br>
2020-07-31 20:25:25 -0300

    Create 1113 - Crescente e Decrescente.cpp

---
+++ Iniciante/1113 - Crescente e Decrescente.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+int main() {
+
+    string resposta;
+    int X, Y;
+    scanf ("%d %d", &X, &Y);
+    while (X!=Y)
+    {
+
+    resposta = (X>Y)?"Decrescente":"Crescente";
+    cout<< resposta<<endl;
+     scanf ("%d %d", &X, &Y);
+
+    }
+
+    return 0;
+}
